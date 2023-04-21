98eff7c7aee903aeff776807ebb276141ee31f4d
Isaac Shepherd <isaac.shepherd1@yahoo.com>
2023-04-21 03:18:03 -0500

    remove algorithm works on initial test case, but not on other test cases. Since other test cases can not be seen it is difficult to pinoint the issue in the algorithm.

--- btNode.cpp
+++ btNode.cpp
@@ -6,7 +6,7 @@ bool bst_insert(btNode*& bst_root, int insInt){
     bool inserted = false;
     btNode* cursor = bst_root;
     if(bst_root == 0){
-        std::cout << "inserting " << insInt << " at root" << std::endl;
+        //std::cout << "inserting " << insInt << " at root" << std::endl;
         bst_root = new btNode;
         bst_root->data = insInt;
         bst_root->left = bst_root->right = 0;
@@ -18,7 +18,7 @@ bool bst_insert(btNode*& bst_root, int insInt){
                 cursor = cursor->left;
             }
             else{
-                std::cout << "inserting " << insInt << " at " << cursor->data << "'s left link" << std::endl;
+                //std::cout << "inserting " << insInt << " at " << cursor->data << "'s left link" << std::endl;
                 cursor->left = new btNode;
                 cursor->left->data = insInt;
                 cursor->left->left = cursor->left->right = 0;
@@ -30,7 +30,7 @@ bool bst_insert(btNode*& bst_root, int insInt){
                 cursor = cursor->right;
             }
             else{
-                std::cout << "inserting " << insInt << " at " << cursor->data << "'s right link" << std::endl;
+                //std::cout << "inserting " << insInt << " at " << cursor->data << "'s right link" << std::endl;
                 cursor->right = new btNode;
                 cursor->right->data = insInt;
                 cursor->right->left = cursor->right->right = 0;
@@ -38,38 +38,43 @@ bool bst_insert(btNode*& bst_root, int insInt){
             }
         }
         else if(insInt == cursor->data){
-            std::cout << "duplicate value, aborting" << std::endl;
-            return false; //do not insert if insInt already exists
+            //std::cout << "duplicate value, aborting" << std::endl;
+            break; //do not insert if insInt already exists
         }
-        //std::cout << "current root is" << bst_root->data << std::endl;
     }
     return inserted;
 }
 // write definition for bst_remove here
 bool bst_remove(btNode*& bst_root,int remInt){
-    std::cout << "rm, ";
+    //std::cout << "rm, ";
     bool removed = false;
     if(bst_root != 0){
+        //std::cout << "node = " << bst_root->data << " target to delete is " << remInt;
         if(remInt < bst_root->data){
+            //std::cout << " going left" << std::endl;
             removed = bst_remove(bst_root->left, remInt);
         }
         else if(remInt > bst_root->data){
+            //std::cout << " going right" << std::endl;
             removed = bst_remove(bst_root->right, remInt);
         }
-        else{
+        else{ //else its equal to remInt, so remove it
             btNode* old_bst_root = bst_root;
             if(bst_root->left == 0 and bst_root->right == 0){ //no children
                 bst_root = 0;
-                old_bst_root = 0;
+                std::cout << " target node " << remInt << " is now: Empty" << std::endl;
             }
-            else if(bst_root->left == 0){ //one child cases
+            else if(bst_root->left == 0 and bst_root->right != 0){ //one child cases
                 bst_root = bst_root->right;
+                std::cout << " target node " << remInt << " is now: " << bst_root->data << std::endl;
             }
-            else if(bst_root->right == 0){
+            else if(bst_root->right == 0 and bst_root->left != 0){
                 bst_root = bst_root->left;
+                std::cout << " target node " << remInt << " is now: " << bst_root->data << std::endl;
             }
-            else{
+            else{ //2 children
                 bst_remove_max(bst_root->left, bst_root->data);
+                std::cout << " target node " << remInt << " is now: " << bst_root->data << std::endl;
             }
             delete old_bst_root;
             removed = true;
@@ -79,7 +84,6 @@ bool bst_remove(btNode*& bst_root,int remInt){
 }
 // write definition for bst_remove_max here
 void bst_remove_max(btNode*& bst_root,int& removed){
-    //std::cout << "called remove_max ";
     if(bst_root->right == 0){
         removed = bst_root->data;
         btNode* old_bst_root = bst_root;
