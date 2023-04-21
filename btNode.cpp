f86a1cc9634776d9142382b086ba6950d00f8b40
Isaac Shepherd <isaac.shepherd1@yahoo.com>
2023-04-20 19:41:56 -0500

    edited functions a few times with no working solutions

--- btNode.cpp
+++ btNode.cpp
@@ -1,38 +1,71 @@
 #include "btNode.h"
+#include <iostream>
 
 // write definition for bst_insert here
-bool bst_insert(btNode*& bst_root,int insInt){
+bool bst_insert(btNode* bst_root, int insInt) {
     bool inserted = false;
-    if(bst_root != 0){
-        if(insInt < bst_root->data){
-            bst_insert(bst_root->left, insInt);
-        }
-        if(insInt > bst_root->data){
-            bst_insert(bst_root->right, insInt);
-        }
-    }
-    else{
+    if(bst_root == 0){
         bst_root = new btNode;
         bst_root->data = insInt;
         bst_root->left = bst_root->right = 0;
-        inserted = true;
+    }
+    else{
+        btNode** cursor = &bst_root;
+        while(!inserted) {
+            if (insInt == cursor->data) {
+                return false;
+            } else if (insInt < cursor->data) {
+                if (cursor->left == 0) {
+                    cursor->left = new btNode;
+                    cursor->left->data = insInt;
+                    cursor->left->left = cursor->left->right = 0;
+                    inserted = true;
+                } else {
+                    cursor = cursor->left;
+                }
+            } else if (insInt > cursor->data) {
+                if (cursor->right == 0) {
+                    cursor->right = new btNode;
+                    cursor->right->data = insInt;
+                    cursor->right->left = cursor->right->right = 0;
+                    inserted = true;
+                } else {
+                    cursor = cursor->right;
+                }
+            }
+        }
     }
     return inserted;
+    /*btNode* cursor = bst_root;
+    while(cursor != 0) {
+        if (insInt < cursor->data) {
+            cursor = cursor->left;
+        } else if (insInt > cursor->data) {
+            cursor = cursor->right;
+        } else {
+            return; //do not insert if insInt already exists
+        }
+    }
+    cursor = new btNode;
+    cursor->data = insInt;
+    cursor->left = cursor->right = 0;*/
 }
 // write definition for bst_remove here
 bool bst_remove(btNode*& bst_root,int remInt){
+    std::cout << "rm, ";
     bool removed = false;
     if(bst_root != 0){
         if(remInt < bst_root->data){
-            bst_remove(bst_root->left, remInt);
+            removed = bst_remove(bst_root->left, remInt);
         }
         else if(remInt > bst_root->data){
-            bst_remove(bst_root->right, remInt);
+            removed = bst_remove(bst_root->right, remInt);
         }
         else{
             btNode* old_bst_root = bst_root;
             if(bst_root->left == 0 and bst_root->right == 0){ //no children
                 bst_root = 0;
+                old_bst_root = 0;
             }
             else if(bst_root->left == 0){ //one child cases
                 bst_root = bst_root->right;
@@ -41,21 +74,25 @@ bool bst_remove(btNode*& bst_root,int remInt){
                 bst_root = bst_root->left;
             }
             else{
-                bst_remove_max(bst_root->left, bst_root->data);//in progress
+                bst_remove_max(bst_root->left, bst_root->data);
             }
             delete old_bst_root;
+            removed = true;
         }
     }
     return removed;
 }
 // write definition for bst_remove_max here
 void bst_remove_max(btNode*& bst_root,int& removed){
-    if(bst_root->right != 0){
-        bst_remove_max(bst_root->right, removed);
+    //std::cout << "called remove_max ";
+    if(bst_root->right == 0){
+        removed = bst_root->data;
+        btNode* old_bst_root = bst_root;
+        bst_root = bst_root->left;
+        delete old_bst_root;
     }
     else{
-        removed = bst_root->data;
-        bst_remove(bst_root, bst_root->data);
+        bst_remove_max(bst_root->right, removed);
     }
 }
 
